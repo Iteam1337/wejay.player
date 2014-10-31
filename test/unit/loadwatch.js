a0a4adbf323a701f1760e7dd578294dbba13de42
Johan Öbrink <johanbrink@spotify.com>
2014-10-31 11:53:30 +0100

    Bunch of crap

---
+++ test/unit/loadwatch.js
@@ -0,0 +1,9 @@
+var chai = require('chai'),
+  expect = chai.expect,
+  sinon = require('sinon'),
+  sinonPromise = require('sinon-promise'),
+  proxyquire = require('proxyquire');
+
+describe('/loadwatch', function () {
+  
+});
