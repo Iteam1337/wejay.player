079e1d61581712d9142821d0a0079da2e89c3928
Rickard <rickard.laurin@gmail.com>
2014-11-19 20:02:18 +0100

    fixed gulp task, added login and search functions

--- src/partials/main/mainCtrl.js
+++ src/partials/main/mainCtrl.js
@@ -1,3 +1,30 @@
-angular.module('wejay.player').controller('MainCtrl', function ($scope) {
-  
+angular.module('wejay.player').controller('MainCtrl', function ($scope, $http) {
+  var socket = io('http://localhost:3000');
+
+  socket.on('connect', function () {
+    console.log('connected');
+  });
+
+  $scope.login = function () {
+    socket.emit('login', $scope.username, $scope.password, function (err, result) {
+      if (err) {
+        console.error(err);
+      }
+      else {
+        console.log(result);
+        $scope.isLoggedIn = true;
+      }
+    });
+  };
+
+  $scope.search = function () {
+    var url = 'https://api.spotify.com/v1/search?type=track&q=' + $scope.query;
+    
+    $http
+      .get(url)
+      .success(function (data) {
+        console.log(data);
+      });
+  };
+
 });
--- test/client/unit/controllers/mainCtrl.js
+++ test/client/unit/controllers/mainCtrl.js
@@ -1,17 +1,40 @@
+var io;
+
 describe('/partials/main/MainCtrl', function () {
 
   var scope, ctrl;
 
   beforeEach(function () {
     module('wejay.player');
+
+    io = sinon.stub().returns({
+      on: sinon.spy(),
+      emit: sinon.spy()
+    });
+
     inject(function ($rootScope, $controller) {
       scope = $rootScope.$new();
       ctrl = $controller('MainCtrl', {$scope: scope});
     });
   });
 
-  xit('should have tests', function () {
-    
+  describe("#login", function() {
+    it("should be a function", function() {
+      expect(scope.login).to.be.a('function');
+    });
+
+    it("should emit a login message to socket", function() {
+      scope.login();
+
+      console.log(io);
+
+      expect(io.defaultBehavior.returnValue.emit).calledOnce.and.calledWith('login');
+    });
   });
 
+  describe("#search", function() {
+    it("should be a function", function() {
+      expect(scope.search).to.be.a('function');
+    });
+  });
 });
